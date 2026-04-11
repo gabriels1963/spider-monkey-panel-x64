fb145a61b872275ba6711d424a8589421ec06d31
marc2k3 <marc2003@live.co.uk>
2026-04-11 12:18:57 +0100

    Update Timer.js

--- dist/samples/basic/Timer.js
+++ dist/samples/basic/Timer.js
@@ -15,25 +15,25 @@ function print_to_console(msg) {
 function on_mouse_lbtn_up() {
 	if (!g_timer_started) {
 		// Timer are created here
-		// 2s - one shot, happens after 2000 ms, only once
+
+		// one shot, happens after 2000 ms
 		g_timer1_ID = setTimeout(() => {
 			// Print and show console
 			fb.ShowConsole();
 			print_to_console('g_timer1: Show console now.');
 		}, 2000);
 
-		// 500ms - periodic, happens every 500 ms
+		// periodic, happens every 500 ms
 		g_timer2_ID = setInterval(() => {
 			g_count++;
 			print_to_console('g_timer2: ' + g_count + ' time(s).');
 		}, 500);
 
 		g_timer_started = true;
-	}
-	else {
+	} else {
 		// Kill all timers
-		clearTimeout(g_timer2_ID);
-		clearInterval(g_timer1_ID);
+		clearTimeout(g_timer1_ID);
+		clearInterval(g_timer2_ID);
 		g_timer_started = false;
 		g_count = 0;
 		print_to_console('Timers killed.');
